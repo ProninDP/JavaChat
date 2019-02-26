37ebaecb8922a26986ea5351e0444a82d1b50521
Dmitry Pronin <slidernode@yandex.ru>
2019-02-26 16:01:34 +0300

    работа над ошибками...

--- src/com/Server/DSSrv.java
+++ src/com/Server/DSSrv.java
@@ -17,7 +17,7 @@ public class DSSrv {
   private static final int PORT = 9996;
 
 
-  public void DSServer() throws IOException { //принимаем данные
+  public void DSServer() { //принимаем данные
     String msg;
     try (ServerSocket server = new ServerSocket(PORT)){
       while (true) {
@@ -30,25 +30,23 @@ public class DSSrv {
         if (msg.equals("ping")) {
           System.out.println("Don't worry, this is ping");
         }
+        in.close();
       }
+
 
     } catch (IOException e){
       e.printStackTrace();
-    } finally {
-      in.close();
-      out.close();
     }
   }
-
-  public void DSClient(InetAddress ipaddr, int port) throws IOException {
+  //TODO: Дописать метод отправки
+  public void DSClient(InetAddress ipaddr, int port) {
     try (Socket socket = new Socket(ipaddr, port)){
       in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
       out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-    }catch (IOException e){
-      e.printStackTrace();
-    }finally {
       in.close();
       out.close();
+    }catch (IOException e){
+      e.printStackTrace();
     }
   }
 
@@ -62,12 +60,11 @@ public class DSSrv {
           out = new BufferedWriter(new OutputStreamWriter(sping.getOutputStream()));
           out.write("ping" + "\n");
           out.flush();
+          out.close();
         } catch (SocketException e){
           System.out.println("Сокет клиента не отвечает, удаляем из списка!");
           receivedNL.delSet(addr); //получатель не доступен, удаляем из списка
-        } //finally {
-          //out.close();
-        //}
+        }
       }
     } catch (IOException e){
       e.printStackTrace();
